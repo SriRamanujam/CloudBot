2a8ebdbb4995fecdf0fdcb600d1a6dea10f90610
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2017-10-08 19:44:00 -0400

    Redid recipe plugin so it works again

--- plugins/recipe.py
+++ plugins/recipe.py
@@ -1,138 +1,53 @@
 """
 recipe.py
 
-Provides commands for searching recipes. Also has a number of commands for returning random recipes
-in multiple formats.
+Ask the internet what you should make for dinner.
 
 Created By:
     - Luke Rogers <https://github.com/lukeroge>
+Heavily modified by:
+    - Sri Ramanujam <https://github.com/sriramanujam>
 
 License:
     GPL v3
 """
 
-import random
-
-import microdata
 import requests
 import bs4
 
 from cloudbot import hook
 from cloudbot.util import web
 
-BASE_URL = "http://www.cookstr.com"
-SEARCH_URL = BASE_URL + "/searches"
-RANDOM_URL = SEARCH_URL + "/surprise"
+WTFSIMFD_URL = "http://whatthefuckshouldimakefordinner.com"
+VEG_SUFFIX = "/veg.php"
 
 # set this to true to censor this plugin!
 CENSOR = True
-PHRASES = [
-    "EAT SOME FUCKING \x02{}\x02",
-    "YOU WON'T NOT MAKE SOME FUCKING \x02{}\x02",
-    "HOW ABOUT SOME FUCKING \x02{}?\x02",
-    "WHY DON'T YOU EAT SOME FUCKING \x02{}?\x02",
-    "MAKE SOME FUCKING \x02{}\x02",
-    "INDUCE FOOD COMA WITH SOME FUCKING \x02{}\x02",
-    "CLASSILY PARTAKE IN SOME FUCKING \x02{}\x02",
-    "COOK UP SOME FUCKING \x02{}\x02",
-    "CURE YOUR MOUTH'S POST TRAUMATIC STRESS DISORDER WITH SOME FUCKING \x02{}\x02",
-    "PROCURE SOME CHILD LABOR TO COOK UP SOME FUCKING \x02{}\x02",
-    "YOUR INDECISION IS FAR LESS APPETIZING THAN SOME FUCKING \x02{}\x02",
-    "PROBABLY FUCK UP SOME FUCKING \x02{}\x02",
-    "LESSEN YOUR MOTHER'S SHAME WITH SOME FUCKING \x02{}\x02",
-    "EAT SHIT, OR IF YOU DON'T LIKE THAT, SOME FUCKING \x02{}\x02"
-]
-
-clean_key = lambda i: i.split("#")[1]
-
 
 class ParseError(Exception):
     pass
 
-
-def get_data(url):
-    """ Uses the metadata module to parse the metadata from the provided URL """
-    try:
-        request = requests.get(url)
-        request.raise_for_status()
-    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
-        raise ParseError(e)
-
-    items = microdata.get_items(request.text)
-
-    for item in items:
-        if item.itemtype == [microdata.URI("http://schema.org/Recipe")]:
-            return item
-
-    raise ParseError("No recipe data found")
-
-
-@hook.command(autohelp=False)
-def recipe(text):
-    """[term] - gets a recipe for [term], or gets a random recipe if no term is specified"""
-    if text:
-        # get the recipe URL by searching
-        try:
-            request = requests.get(SEARCH_URL, params={'query': text.strip()})
-            request.raise_for_status()
-        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
-            return "Could not get recipe: {}".format(e)
-
-        search = bs4.BeautifulSoup(request.text)
-
-        # find the list of results
-        result_list = search.find('div', {'class': 'found_results'})
-
-        if result_list:
-            results = result_list.find_all('div', {'class': 'recipe_result'})
-        else:
-            return "No results"
-
-        # pick a random front page result
-        result = random.choice(results)
-
-        # extract the URL from the result
-        url = BASE_URL + result.find('div', {'class': 'image-wrapper'}).find('a')['href']
-
-    else:
-        # get a random recipe URL
-        try:
-            request = requests.get(RANDOM_URL)
-            request.raise_for_status()
-        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
-            return "Could not get recipe: {}".format(e)
-
-        url = request.url
-
-    # use get_data() to get the recipe info from the URL
-    try:
-        data = get_data(url)
-    except ParseError as e:
-        return "Could not parse recipe: {}".format(e)
-
-    name = data.name.strip()
-    return "Try eating \x02{}!\x02 - {}".format(name, web.try_shorten(url))
-
-
 # inspired by http://whatthefuckshouldimakefordinner.com/ <3
 @hook.command("dinner", "wtfsimfd", autohelp=False)
-def dinner():
-    """- TELLS YOU WHAT THE F**K YOU SHOULD MAKE FOR DINNER"""
+def dinner(text):
+    """<veg> - TELLS YOU WHAT THE F**K YOU SHOULD MAKE FOR DINNER"""
     try:
-        request = requests.get(RANDOM_URL)
+        request = requests.get(WTFSIMFD_URL + (VEG_SUFFIX if text == 'veg' else ''))
         request.raise_for_status()
     except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
-        return "I CANT GET A DAMN RECIPE: {}".format(e).upper()
+        return "I CAN'T GET A DAMN RECIPE: {}".format(e).upper()
 
     url = request.url
 
     try:
-        data = get_data(url)
+        soup = bs4.BeautifulSoup(request.text)
+        divs = soup.find_all('dl')
+        intro = divs[0].text.strip()
+        recipe = divs[1].text.strip()
+        url = divs[1].a['href']
+        text = (intro + ' ' + recipe).upper()
     except ParseError as e:
-        return "I CANT READ THE F**KING RECIPE: {}".format(e).upper()
-
-    name = data.name.strip().upper()
-    text = random.choice(PHRASES).format(name)
+        return "I CAN'T READ THE F**KING RECIPE: {}".format(e).upper()
 
     if CENSOR:
         text = text.replace("FUCK", "F**K")
