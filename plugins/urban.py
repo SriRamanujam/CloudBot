f8b8115e375b6061c0ec270715888937a1fab51b
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2017-10-08 23:52:33 -0400

    ud for lyfe

--- plugins/urban.py
+++ plugins/urban.py
@@ -11,7 +11,7 @@ define_url = base_url + "/define"
 random_url = base_url + "/random"
 
 
-@hook.command("urban", "u", autohelp=False)
+@hook.command("urban", "ud", autohelp=False)
 def urban(text):
     """urban <phrase> [id] -- Looks up <phrase> on urbandictionary.com."""
 
