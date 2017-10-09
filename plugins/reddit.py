99de8419fd10ac99d08fd0971e4deca0b38b3c37
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2017-10-08 21:07:17 -0400

    Initial reformat/rewrite of reddit plugin

--- plugins/reddit.py
+++ plugins/reddit.py
@@ -4,6 +4,7 @@ import random
 import asyncio
 import functools
 import urllib.parse
+from html.parser import HTMLParser
 
 import requests
 
@@ -14,10 +15,34 @@ from cloudbot.util import timeformat, formatting
 reddit_re = re.compile(r'.*(((www\.)?reddit\.com/r|redd\.it)[^ ]+)', re.I)
 
 base_url = "http://reddit.com/r/{}/.json"
+top_url = "http://reddit.com/r/{}/top/.json?t={}"
 short_url = "http://redd.it/{}"
 
 
-def format_output(item, show_url=False):
+def format_output(item, show_url=False, prefix=None):
+    """ takes a reddit post and returns a formatted string"""
+    item['prefix'] = prefix
+    raw_time = datetime.fromtimestamp(int(item['created_utc']))
+    item['timesince'] = timeformat.time_since(raw_time, count=1, simple=True)
+    if item['is_self'] is False or show_url is True:
+        item['link'] = short_url.format(item['id'])
+    else:
+        item['link'] = ''
+
+    item['title'] = HTMLParser().unescape(item['title'])
+    item['comments'] = formatting.pluralize(item['num_comments'], 'comment')
+    item['upvotes'] = formatting.pluralize(item['score'], 'upvote')
+
+    if item['over_18']:
+        item['warning'] = ' \x037|\x03 \x02NSFW\x02'
+    else:
+        item['warning'] = ''
+
+    return '{prefix}{title} by \x02u/{author}\x02 \x037|\x03 {upvotes}, {comments} \x037|\x03 Submitted {timesince} ago \x037|\x03 {link}{warning}'.format(**item)
+
+
+
+def format_output_old(item, show_url=False):
     """ takes a reddit post and returns a formatted string """
     item["title"] = formatting.truncate(item["title"], 70)
     item["link"] = short_url.format(item["id"])
@@ -61,24 +86,36 @@ def reddit_url(match, bot):
 
 
 @asyncio.coroutine
-@hook.command(autohelp=False)
+@hook.command('reddit', 'r', autohelp=False)
 def reddit(text, bot, loop):
-    """<subreddit> [n] - gets a random post from <subreddit>, or gets the [n]th post in the subreddit"""
+    """<subreddit> [n] <time> - for time period <time>, gets a random post from <subreddit>, or gets the [n]th post in the subreddit"""
     id_num = None
+    time_period = ''
+    prefix=None
+    parts = []
     headers = {'User-Agent': bot.user_agent}
 
     if text:
         # clean and split the input
         parts = text.lower().strip().split()
 
-        # find the requested post number (if any)
+        # find the requested post number and time period (if any)
         if len(parts) > 1:
-            url = base_url.format(parts[0].strip())
             try:
                 id_num = int(parts[1]) - 1
             except ValueError:
                 return "Invalid post number."
+
+            url = base_url.format(parts[0].strip())
+            try:
+                time_period = parts[2].strip()
+                if time_period not in ['hour', 'day', 'week', 'month', 'year', 'all']:
+                    return "Invalid time period."
+                url = top_url.format(parts[0].strip(), time_period)
+            except IndexError:
+                pass
         else:
+            print(parts[0])
             url = base_url.format(parts[0].strip())
     else:
         url = "http://reddit.com/.json"
@@ -101,4 +138,12 @@ def reddit(text, bot, loop):
     else:
         item = random.choice(data)["data"]
 
-    return format_output(item, show_url=True)
+    if len(parts) > 1:
+        prefix = 'Top reddit post in \x02r/{}\x02'.format(parts[0].strip())
+        if time_period == 'all':
+            prefix += ' of all time'
+        elif time_period != '':
+            prefix += ' in the past {}'.format(time_period[3:])
+        prefix += ' \x037|\x03 '
+
+    return format_output(item, show_url=True, prefix=prefix)
