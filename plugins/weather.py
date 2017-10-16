a213ec704c7b3b035db282e248b46db0e713345d
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2017-10-15 23:09:57 -0400

    Get weather locations out of the db like the old days

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,7 +1,9 @@
 import requests
 
+from sqlalchemy import Table, Column, PrimaryKeyConstraint, String
+
 from cloudbot import hook
-from cloudbot.util import web
+from cloudbot.util import web, database
 
 
 class APIError(Exception):
@@ -11,12 +13,21 @@ class APIError(Exception):
 google_base = 'https://maps.googleapis.com/maps/api/'
 geocode_api = google_base + 'geocode/json'
 
-forecast_io_api = "https://api.forecast.io/forecast/{}/{}?units={}"
+forecast_io_api = "https://api.forecast.io/forecast/{}/{}?units=us"
+
+table = Table(
+    "weather",
+    database.metadata,
+    Column('nick', String(255)),
+    Column('location', String(255)),
+    PrimaryKeyConstraint('nick')
+)
 
 # Change this to a ccTLD code (eg. uk, nz) to make results more targeted towards that specific country.
 # <https://developers.google.com/maps/documentation/geocoding/#RegionCodes>
 bias = None
 
+w_cache = []
 
 def check_status(status):
     """
@@ -76,30 +87,47 @@ def _parse_weather_output(weather_data, location):
     return "Current weather in \x02{location}\x02 \x02\x033|\x03\x02 {summary}, {temp_f}°F ({temp_c}°C) feels like {feelslike_f}°F ({feelslike_c}°C), {humidity}% humidity, wind speed {windspeed_mph} mph ({windspeed_ms} m/s), {precip_chance}% chance of precipitation. Today's forecast: {daily_summary}".format(**w_dict)
 
 
+def load_weather_db(db):
+    global w_cache
+    w_cache = []
+    for row in db.execute(table.select()):
+        w_cache.append((row['nick'], row['location']))
+
+
 @hook.on_start
-def on_start(bot):
+def load_cache(bot, db):
     """ Loads API keys """
     global dev_key, forecast_io_key
     dev_key = bot.config.get("api_keys", {}).get("google_dev_key", None)
     forecast_io_key = bot.config.get("api_keys", {}).get("forecast_io", None)
+    load_weather_db(db)
+
+
+def get_saved_weather(nick):
+    global w_cache
+    w_loc = [row[1] for row in w_cache if nick.lower() == row[0]]
+    return w_loc
 
 
 @hook.command("weather", "w", autohelp=False)
-def weather(text, reply):
-    """weather <location>:<units>. Units can be one of [us, si, ca, uk, both]. Defaults to us"""
+def weather(text, reply, bot, db, nick, notice):
+    """weather <location> --save. If you pass --save, the location will be saved to the database. You can get your weather for your saved location by passing .weather without any parameters.
+    """
     if not forecast_io_key:
         return "This command requires a forecast.io API key."
     if not dev_key:
         return "This command requires a Google Developers Console API key."
 
-    if not text:
-        return "Saved weather coming soon! Hold on to your butts."
-    else:
-        parts = text.split(':')
-        location, *measure_type = parts
+    should_save = text.endswith(' --save')
 
-    if measure_type and measure_type[0] not in ['us', 'si', 'ca', 'uk', 'both']:
-        return "Invalid units. Please pick from one of [us, si, ca, uk, both]."
+    if not text:
+        # try to find nick in database
+        location = get_saved_weather(nick)
+        if not location:
+            notice(weather.__doc__)
+            return
+    else:
+        location = text.split(' --')[0]
 
     # use find_location to get location data from the user input
     try:
@@ -109,9 +137,16 @@ def weather(text, reply):
 
     formatted_location = "{lat},{lng}".format(**location_data)
 
-    url = forecast_io_api.format(forecast_io_key, formatted_location,
-            measure_type[0] if measure_type else 'us')
+    url = forecast_io_api.format(forecast_io_key, formatted_location)
     response = requests.get(url).json()
 
     reply(_parse_weather_output(response, formatted_address))
+
+    if should_save:
+        db.execute('insert into weather(nick, location) values (:nick, :location) '
+                'on conflict(nick) do update set location = EXCLUDED.location',
+                {'nick': nick.lower(), 'location': formatted_address})
+        db.commit()
+        load_weather_db(db)
+        notice('Location {} saved'.format(formatted_address))
 
